184b5fc756260173ff4cf5318226c3f4b10e646b
thisisaaronland <devnull@localhost>
2020-11-06 14:32:26 -0500

    chipping away - fatal errors creating windows that I don't understand

--- MultiScreen/AppDelegate.swift
+++ MultiScreen/AppDelegate.swift
@@ -1,28 +1,33 @@
-//
-//  AppDelegate.swift
-//  MultiScreen
-//
-//  Created by asc on 11/6/20.
-//
-
 import UIKit
 import WebKit
 import Foundation
+import Logging
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    private let primary = "main.html"
-    private let external = "external.html"
+    
+    // https://developer.apple.com/documentation/uikit/windows_and_screens
+    // https://developer.apple.com/documentation/uikit/uiscreen
+    // https://developer.apple.com/documentation/uikit/windows_and_screens/displaying_content_on_a_connected_screen
+    
+    // https://www.bignerdranch.com/blog/adding-external-display-support-to-your-ios-app-is-ridiculously-easy/
+    // https://medium.com/@dmytro.anokhin/notification-in-swift-d47f641282fa
+    
+    var logger = Logger(label: "org.sfomuseum.multiscreen")
     
     var window: UIWindow?
-
+    
+    
     // References to our windows that we're creating
+    
     var windowsForScreens = [UIScreen: UIWindow]()
     
     // Create our view controller and add text to our test label
     
     private func addViewController(to window: UIWindow, requestURLString: String) {
+        
+        self.logger.info("add view controller \(requestURLString)")
+        
         let vc = ViewController.makeFromStoryboard(requestURLString: requestURLString)
         
         vc.loadViewIfNeeded()
@@ -30,17 +35,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     // Create and set up a new window with our view controller as the root
+    
     private func setupWindow(for screen: UIScreen) {
+        
         let window = UIWindow()
         
-        addViewController(to: window, requestURLString: external)
+        let requestURLString = "external.html"
+        addViewController(to: window, requestURLString: requestURLString)
         
         window.screen = screen
         window.makeKeyAndVisible()
         
         windowsForScreens[screen] = window
     }
-
+    
     // Hide the window and remove our reference to it so it will be deallocated
     
     private func tearDownWindow(for screen: UIScreen) {
@@ -48,14 +56,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.isHidden = true
         windowsForScreens[screen] = nil
     }
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        self.logger.logLevel = .warning
         
         // Set up the device's main screen UI
-        addViewController(to: window!, requestURLString: primary)
+        
+        let requestURLString = "main.html"
+        addViewController(to: window!, requestURLString: requestURLString)
         
         // We need to set up the other screens that are already connected
+        
         let otherScreens = UIScreen.screens.filter { $0 != UIScreen.main }
         
         otherScreens.forEach { (screen) in
@@ -64,6 +78,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Listen for the screen connection notification
         // then set up the new window and attach it to the screen
+        
         NotificationCenter.default
             .addObserver(forName: UIScreen.didConnectNotification,
                          object: nil,
@@ -76,6 +91,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         // Listen for the screen disconnection notification.
+        
         NotificationCenter.default.addObserver(forName: UIScreen.didDisconnectNotification,
                                                object: nil,
                                                queue: .main) { (notification) in
@@ -84,37 +100,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                                 self.tearDownWindow(for: newScreen)
         }
         
-        /*
- 
-        NotificationCenter.default.addObserver(forName: Notification.Name(rawValue: "setFocus"),
-                                               object: nil,
-                                               queue: .main) { (notification) in
-                                                let msg = notification.object as! WKScriptMessage
-                                                
-                                                if let focus = msg.body as? String {
-                                                    // print("UPDATE FOCUS", focus)
-                                                    self.current_location.focus = focus
-                                                }
-        }
-        */
-        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+    var versionNumberPretty: String {
+        return "v\(releaseVersionNumber ?? "0.0.0") (v\(buildVersionNumber ?? "x"))"
+    }
+}
