9f246b5e4f16f24f49bf14644999d3f1139064d9
whosonfirst <whosonfirst@localhost>
2022-10-21 06:58:51 -0700

    snapsoht: more settings wrangling

--- MultiScreen/AppDelegate.swift
+++ MultiScreen/AppDelegate.swift
@@ -12,9 +12,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     /// The URL of the "controller" application defined in ...
     var relay_endpoint: URL?
     
-    /// The URL of the "controller" application's "checkin" endpoint. We use this to signal that the iPad is still up and running.
-    var checkin_endpoint: URL?
-    
     /// The URL of the "controller" application's "SSE" endpoint where this application will listen for events to control the map
     var sse_endpoint: URL?
     
@@ -23,12 +20,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var sse_enable = true   // Read from info.plist
 
+    // I wish something like were just built in to iOS...
+    // https://github.com/Abstract45/SettingsExample
+    
+    private func registerSettingsBundle() {
+        
+        guard let settingsBundle = Bundle.main.url(forResource: "Settings", withExtension:"bundle") else {
+            NSLog("Could not find Settings.bundle")
+            return;
+        }
+        
+        guard let settings = NSDictionary(contentsOf: settingsBundle.appendingPathComponent("Root.plist")) else {
+            NSLog("Could not find Root.plist in settings bundle")
+            return
+        }
+        
+        guard let preferences = settings.object(forKey: "PreferenceSpecifiers") as? [[String: AnyObject]] else {
+            NSLog("Root.plist has invalid format")
+            return
+        }
+        
+        var defaultsToRegister = [String: AnyObject]()
+        for p in preferences {
+            if let k = p["Key"] as? String, let v = p["DefaultValue"] {
+                // NSLog("%@", "registering \(v) for key \(k)")
+                defaultsToRegister[k] = v
+            }
+        }
+        
+        UserDefaults.standard.register(defaults: defaultsToRegister)
+    }
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
 
         logger.logLevel = .debug
         
+        // START OF stuff defined in Setttings.bundle
+        
+        registerSettingsBundle()
+        
+        let settings = UserDefaults.standard
+        settings.synchronize()
+        
+        sse_enable = settings.bool(forKey: "EnableRelay")
+        let str_relay_endpoint = settings.string(forKey: "RelayEndpoint")
+        
+        if str_relay_endpoint == nil{
+            () // FIX ME
+        }
+        
+        relay_endpoint = URL(string: str_relay_endpoint!)
+        
+        if relay_endpoint == nil {
+            () // FIX ME
+        }
         //MARK:  Check network status
         // https://developer.apple.com/documentation/network/nwpathmonitor/2998733-init
         
